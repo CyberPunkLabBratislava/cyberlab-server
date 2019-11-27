09900d4cc194d3a939e302be64eb6a9d6a018091
Jorge Almela <jorgealmela@JorgePc.local>
2019-11-27 23:50:11 +0100

    Insert image in db first version

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 // Import packages
 var express = require('express');
 var mongoose = require('mongoose');
+var multer = require('multer');
+var upload = multer({ dest:'uploads/'});
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+var http = require('http');
+var fs = require('fs');
 
 // Import configuration
 var port = process.env.PORT;
@@ -9,11 +16,53 @@ var mongoString = process.env.MONGO_STRING;
 // Instantiate server app
 var app = express();
 
+// Middlewares
+app.use(cors({origin:'*'}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// Mongo Models
+var Schema = mongoose.Schema;
+var picture = new Schema({
+    date: { type: Date, default: Date.now },
+    image: { data: Buffer, contentType: String, size: String }
+});
+var pictureModel = mongoose.model('picture', picture);
+
+
 // Endpoints
 app.get('/', (req, res) => res.send('Welcome to cyberlab server!'));
 app.post('/', function(req, res){
   var body = req.body;
   res.json({error: 'false', msg: 'We got your message', payload: body});
+})
+app.post('/image', upload.single('picture'), function(req, res){
+  if(req.file){
+    // read the img file from tmp in-memory location
+    var newImg = fs.readFileSync(req.file.path);
+    // encode the file as a base64 string.
+    var encImg = newImg.toString('base64');
+    // define your new document
+    var newItem = {
+       // description: req.body.description,
+       contentType: req.file.mimetype,
+       size: req.file.size,
+       data: Buffer(encImg, 'base64')
+    };
+    var pictureDb = new pictureModel({image: newItem});
+    pictureDb.save()
+    .then(function(response){
+      res.json({error: false, msg: "Image received", desc: req.file});
+    })
+    .catch(function(err){
+      console.log(err);
+      res.json({error: true, msg: "Error storing image...", desc: err});
+    });
+  } else {
+    res.json({error: true, msg: "Missing image..."});
+  }
 })
 
 // CONNECTING to MONGO
@@ -39,5 +88,9 @@ var mongoConnect = function(){
 
 mongoConnect();
 
-// Start server
-app.listen(port, () => console.log("Server listening on port " + port))
+// START SERVER
+var server = http.createServer(app);
+server.listen(port, () => console.log("Server listening on port " + port));
+
+// Export app module
+module.exports = app;
