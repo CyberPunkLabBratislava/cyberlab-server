f779b6f48d0936547b36853a4681a88aac7bc311
Jorge Almela <jorgealmela@JorgePc.local>
2020-02-16 11:36:57 +0100

    Fix integration issues with ui app

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "cyberlabserver",
-    script: "./server/index.js",
+    script: "index.js",
     output: './logs/out.log',
     error: './logs/error.log',
     log: './logs/combined.outerr.log',
@@ -13,7 +13,7 @@ module.exports = {
       NODE_ENV: "development",
       PORT: 3000,
       MONGO_STRING: "mongodb://mongodb:27017/db",
-      AI_URL: "http://localhost:5000/",
+      AI_URL: "http://parking-classifier:5000/",
       UPLOAD_PATH: "uploads/"
     }
   }]
