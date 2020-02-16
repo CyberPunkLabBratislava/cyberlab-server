b95e92be84f77dc5efe85b23aa2cd93ffe77740e
Jorge Almela <jorgealmela@JorgePc.local>
2020-02-16 14:50:39 +0100

    Added mongo model for thing

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# This scripts relies on the image parking-classifier being installed in the server
 docker-compose down
 mkdir -p logs/server
 mkdir -p logs/mongo
--- server/classes/models.js
+++ server/classes/models.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var uuid = require('uuid/v1');
 
 var picture = new Schema({
     date: { type: Date, default: Date.now },
@@ -12,6 +13,18 @@ var picture = new Schema({
     }
 });
 
+var thing = new Schema({
+    created: { type: Date, default: Date.now },
+    mode: { type: String, required: true },
+    type: { type: String, required: true },
+    id: { type: String, default: uuid() }
+    // mac
+    // hasItems
+    // authentication
+});
+
 picture.set('autoIndex',true);
+thing.set('autoIndex',true);
 
 module.exports.picture = mongoose.model('picture', picture);
+module.exports.thing = mongoose.model('thing', thing);
